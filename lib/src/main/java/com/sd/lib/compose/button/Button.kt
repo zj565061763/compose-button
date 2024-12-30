b99ab38ea2680e6d7ee05b30a582aa7e65b4dfce
zjun <565061763@qq.com>
2024-12-30 21:47:01 +0800

    小调整

--- lib/src/main/java/com/sd/lib/compose/button/Button.kt
+++ lib/src/main/java/com/sd/lib/compose/button/Button.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.LocalContentColor
@@ -33,100 +34,98 @@ import androidx.compose.ui.unit.dp
  */
 @Composable
 fun FTextButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    shape: Shape = ButtonDefaults.textShape,
-    colors: ButtonColors = ButtonDefaults.textButtonColors(
-        contentColor = LocalContentColor.current,
-        disabledContentColor = LocalContentColor.current.copy(0.38f),
-    ),
-    border: BorderStroke? = null,
-    contentPadding: PaddingValues = PaddingValues(horizontal = 10.dp, vertical = 5.dp),
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    content: @Composable RowScope.() -> Unit,
-) =
-    FButton(
-        onClick = onClick,
-        modifier = modifier,
-        enabled = enabled,
-        shape = shape,
-        colors = colors,
-//        elevation = elevation,
-        border = border,
-        contentPadding = contentPadding,
-        interactionSource = interactionSource,
-        content = content
-    )
+  onClick: () -> Unit,
+  modifier: Modifier = Modifier,
+  enabled: Boolean = true,
+  shape: Shape = ButtonDefaults.textShape,
+  colors: ButtonColors = ButtonDefaults.textButtonColors(
+    contentColor = LocalContentColor.current,
+    disabledContentColor = LocalContentColor.current.copy(0.38f),
+  ),
+  border: BorderStroke? = null,
+  contentPadding: PaddingValues = PaddingValues(horizontal = 8.dp, vertical = 4.dp),
+  interactionSource: MutableInteractionSource? = null,
+  content: @Composable RowScope.() -> Unit,
+) {
+  FButton(
+    onClick = onClick,
+    modifier = modifier,
+    enabled = enabled,
+    shape = shape,
+    colors = colors,
+    border = border,
+    contentPadding = contentPadding,
+    interactionSource = interactionSource,
+    content = content
+  )
+}
 
 /**
- * Button
+ * [Button]
  */
 @Composable
 fun FButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    shape: Shape = ButtonDefaults.shape,
-    colors: ButtonColors = ButtonDefaults.buttonColors(),
-    border: BorderStroke? = null,
-    contentPadding: PaddingValues = PaddingValues(horizontal = 10.dp, vertical = 5.dp),
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    content: @Composable RowScope.() -> Unit,
+  onClick: () -> Unit,
+  modifier: Modifier = Modifier,
+  enabled: Boolean = true,
+  shape: Shape = ButtonDefaults.shape,
+  colors: ButtonColors = ButtonDefaults.buttonColors(),
+  border: BorderStroke? = null,
+  contentPadding: PaddingValues = PaddingValues(horizontal = 8.dp, vertical = 4.dp),
+  interactionSource: MutableInteractionSource? = null,
+  content: @Composable RowScope.() -> Unit,
 ) {
-    val containerColor = colors.containerColorExt(enabled)
-    val contentColor = colors.contentColorExt(enabled)
-//    val shadowElevation = elevation?.shadowElevation(enabled, interactionSource)?.value ?: 0.dp
-//    val tonalElevation = elevation?.tonalElevation(enabled) ?: 0.dp
-    FSurface(
-        onClick = onClick,
-        modifier = modifier.semantics { role = Role.Button },
-        enabled = enabled,
-        shape = shape,
-        color = containerColor,
-        contentColor = contentColor,
-//        tonalElevation = tonalElevation,
-//        shadowElevation = shadowElevation,
-        border = border,
-        interactionSource = interactionSource
+  @Suppress("NAME_SHADOWING")
+  val interactionSource = interactionSource ?: remember { MutableInteractionSource() }
+  val containerColor = colors.containerColorExt(enabled)
+  val contentColor = colors.contentColorExt(enabled)
+  InternalSurface(
+    onClick = onClick,
+    modifier = modifier.semantics { role = Role.Button },
+    enabled = enabled,
+    shape = shape,
+    color = containerColor,
+    contentColor = contentColor,
+    border = border,
+    interactionSource = interactionSource
+  ) {
+    ProvideContentColorTextStyle(
+      contentColor = contentColor,
+      textStyle = MaterialTheme.typography.labelLarge
     ) {
-        ProvideContentColorTextStyle(
-            contentColor = contentColor,
-            textStyle = MaterialTheme.typography.labelLarge
-        ) {
-            Row(
-                Modifier
-                    .defaultMinSize(
-                        minWidth = 60.dp,
-                        minHeight = 30.dp,
-                    )
-                    .padding(contentPadding),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
-                content = content
-            )
-        }
+      Row(
+        Modifier
+          .defaultMinSize(
+            minWidth = 48.dp,
+            minHeight = 24.dp,
+          )
+          .padding(contentPadding),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        content = content
+      )
     }
+  }
 }
 
 @Stable
 private fun ButtonColors.containerColorExt(enabled: Boolean): Color =
-    if (enabled) containerColor else disabledContainerColor
+  if (enabled) containerColor else disabledContainerColor
 
 @Stable
 private fun ButtonColors.contentColorExt(enabled: Boolean): Color =
-    if (enabled) contentColor else disabledContentColor
+  if (enabled) contentColor else disabledContentColor
 
 @Composable
 private fun ProvideContentColorTextStyle(
-    contentColor: Color,
-    textStyle: TextStyle,
-    content: @Composable () -> Unit,
+  contentColor: Color,
+  textStyle: TextStyle,
+  content: @Composable () -> Unit,
 ) {
-    val mergedStyle = LocalTextStyle.current.merge(textStyle)
-    CompositionLocalProvider(
-        LocalContentColor provides contentColor,
-        LocalTextStyle provides mergedStyle,
-        content = content
-    )
+  val mergedStyle = LocalTextStyle.current.merge(textStyle)
+  CompositionLocalProvider(
+    LocalContentColor provides contentColor,
+    LocalTextStyle provides mergedStyle,
+    content = content
+  )
 }
--- lib/src/main/java/com/sd/lib/compose/button/Surface.kt
+++ lib/src/main/java/com/sd/lib/compose/button/Surface.kt
@@ -6,19 +6,18 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.LocalAbsoluteTonalElevation
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.LocalTonalElevationEnabled
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.contentColorFor
+import androidx.compose.material3.ripple
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.Stable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -30,72 +29,78 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
-internal fun FSurface(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    enabled: Boolean = true,
-    shape: Shape = RectangleShape,
-    color: Color = MaterialTheme.colorScheme.surface,
-    contentColor: Color = contentColorFor(color),
-    tonalElevation: Dp = 0.dp,
-    shadowElevation: Dp = 0.dp,
-    border: BorderStroke? = null,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    content: @Composable () -> Unit,
+internal fun InternalSurface(
+  onClick: () -> Unit,
+  modifier: Modifier = Modifier,
+  enabled: Boolean = true,
+  shape: Shape = RectangleShape,
+  color: Color = MaterialTheme.colorScheme.surface,
+  contentColor: Color = contentColorFor(color),
+  tonalElevation: Dp = 0.dp,
+  shadowElevation: Dp = 0.dp,
+  border: BorderStroke? = null,
+  interactionSource: MutableInteractionSource? = null,
+  content: @Composable () -> Unit,
 ) {
-    val absoluteElevation = LocalAbsoluteTonalElevation.current + tonalElevation
-    CompositionLocalProvider(
-        LocalContentColor provides contentColor,
-        LocalAbsoluteTonalElevation provides absoluteElevation
+  val absoluteElevation = LocalAbsoluteTonalElevation.current + tonalElevation
+  CompositionLocalProvider(
+    LocalContentColor provides contentColor,
+    LocalAbsoluteTonalElevation provides absoluteElevation
+  ) {
+    Box(
+      modifier = modifier
+        .surface(
+          shape = shape,
+          backgroundColor = surfaceColorAtElevation(color = color, elevation = absoluteElevation),
+          border = border,
+          shadowElevation = with(LocalDensity.current) { shadowElevation.toPx() }
+        )
+        .clickable(
+          interactionSource = interactionSource,
+          indication = ripple(),
+          enabled = enabled,
+          onClick = onClick
+        ),
+      propagateMinConstraints = true
     ) {
-        Box(
-            modifier = modifier
-//                .minimumInteractiveComponentSize()
-                .surface(
-                    shape = shape,
-                    backgroundColor = surfaceColorAtElevation(
-                        color = color,
-                        elevation = absoluteElevation
-                    ),
-                    border = border,
-                    shadowElevation = with(LocalDensity.current) { shadowElevation.toPx() }
-                )
-                .clickable(
-                    interactionSource = interactionSource,
-                    indication = rememberRipple(),
-                    enabled = enabled,
-                    onClick = onClick
-                ),
-            propagateMinConstraints = true
-        ) {
-            content()
-        }
+      content()
     }
+  }
 }
 
 @Stable
 private fun Modifier.surface(
-    shape: Shape,
-    backgroundColor: Color,
-    border: BorderStroke?,
-    shadowElevation: Float,
+  shape: Shape,
+  backgroundColor: Color,
+  border: BorderStroke?,
+  shadowElevation: Float,
 ) = this
-    .graphicsLayer(shadowElevation = shadowElevation, shape = shape, clip = false)
-    .then(if (border != null) Modifier.border(border, shape) else Modifier)
-    .background(color = backgroundColor, shape = shape)
-    .clip(shape)
+  .then(
+    if (shadowElevation > 0f) {
+      Modifier.graphicsLayer(
+        shadowElevation = shadowElevation,
+        shape = shape,
+        clip = false
+      )
+    } else {
+      Modifier
+    }
+  )
+  .then(if (border != null) Modifier.border(border, shape) else Modifier)
+  .background(color = backgroundColor, shape = shape)
+  .clip(shape)
 
 @Composable
 private fun surfaceColorAtElevation(color: Color, elevation: Dp): Color =
-    MaterialTheme.colorScheme.applyTonalElevation(color, elevation)
+  MaterialTheme.colorScheme.applyTonalElevation(color, elevation)
 
 @Composable
 @ReadOnlyComposable
 private fun ColorScheme.applyTonalElevation(backgroundColor: Color, elevation: Dp): Color {
-    val tonalElevationEnabled = LocalTonalElevationEnabled.current
-    return if (backgroundColor == surface && tonalElevationEnabled) {
-        surfaceColorAtElevation(elevation)
-    } else {
-        backgroundColor
-    }
+  val tonalElevationEnabled = LocalTonalElevationEnabled.current
+  return if (backgroundColor == surface && tonalElevationEnabled) {
+    surfaceColorAtElevation(elevation)
+  } else {
+    backgroundColor
+  }
 }
