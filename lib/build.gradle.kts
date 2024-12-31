630490ba39a760be942056288f8d5560d1b65f2a
zjun <565061763@qq.com>
2024-12-31 08:45:52 +0800

    1.1.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-button"
-val libVersion = "1.0.0"
+val libVersion = "1.1.0"
 
 android {
   namespace = "com.sd.lib.compose.button"
